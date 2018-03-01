c3de8ed34d2640ea950b3e39b66f9c5bcc37fd02
Dmitriy <pichugind@gmail.com>
2018-03-01 18:14:29 +0300

    Zepplin extension

---
+++ Privratnik/UIExtensions/UIColor+Additions.swift
@@ -0,0 +1,49 @@
+//
+//  UIColor+Additions.swift
+//  ПривратникЪ
+//
+//  Generated on Zeplin. (01.03.2018).
+//  Copyright (c) 2018 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteTwo: UIColor {
+    return UIColor(white: 233.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var algaeGreen: UIColor {
+    return UIColor(red: 36.0 / 255.0, green: 191.0 / 255.0, blue: 61.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var greyishBrown: UIColor {
+    return UIColor(white: 71.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteThree: UIColor {
+    return UIColor(white: 216.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var leather: UIColor {
+    return UIColor(red: 181.0 / 255.0, green: 139.0 / 255.0, blue: 58.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var warmGrey: UIColor {
+    return UIColor(white: 140.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var duskBlue: UIColor {
+    return UIColor(red: 40.0 / 255.0, green: 53.0 / 255.0, blue: 126.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brick: UIColor {
+    return UIColor(red: 153.0 / 255.0, green: 40.0 / 255.0, blue: 40.0 / 255.0, alpha: 1.0)
+  }
+
+}
