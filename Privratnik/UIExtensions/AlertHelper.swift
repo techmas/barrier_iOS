c348c807987d81871e4245e164fab1128e51bbcf
Dmitriy <pichugind@gmail.com>
2018-03-16 12:08:29 +0300

    UIAlertView

---
+++ Privratnik/UIExtensions/AlertHelper.swift
@@ -0,0 +1,21 @@
+//
+//  AlertHelper.swift
+//  Privratnik
+//
+//  Created by Дмитрий Пичугин on 16.03.2018.
+//  Copyright © 2018 Techmas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+
+    func displayAlert(_ message: String = "Не удалось выполнить операцию.") {
+        let alertController = UIAlertController(title: "Внимание!", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
+}
